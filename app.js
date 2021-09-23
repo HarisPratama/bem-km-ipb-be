e392dbd9f826c2daf2bde467aaf67b551bfc81b5
HarisPratama <hpratama324@gmail.com>
2021-09-23 13:40:36 +0700

    2

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
-    res.status(200).json('BEM KM IPB API')
+    res.status(200).json('BEM KM IPB API ')
 })
 app.use(router)
 
