22caa57119a8045641a9488af59b1bf87b900738
HarisPratama <hpratama324@gmail.com>
2022-07-25 17:57:54 +0700

    update

--- routers/news.js
+++ routers/news.js
@@ -1,13 +1,14 @@
-const NewsController = require('../controllers/news')
-const { uploadImage } = require('../middlewares/multer')
+const NewsController = require('../controllers/news');
+const { uploadImage } = require('../middlewares/multer');
 
-const router = require('express').Router()
+const router = require('express').Router();
 
-router.get('/', NewsController.getNews)
-router.post('/', uploadImage, NewsController.postNews)
-router.get('/filter', NewsController.getFilterNews)
-router.post('/comment', NewsController.postComment)
-router.get('/:id', NewsController.getDetailNews)
-router.delete('/:id', NewsController.deleteNews)
+router.get('/', NewsController.getNews);
+router.post('/', uploadImage, NewsController.postNews);
+router.get('/filter', NewsController.getFilterNews);
+router.post('/comment', NewsController.postComment);
+router.put('/:id', NewsController.updateNews);
+router.get('/:id', NewsController.getDetailNews);
+router.delete('/:id', NewsController.deleteNews);
 
-module.exports = router
+module.exports = router;
